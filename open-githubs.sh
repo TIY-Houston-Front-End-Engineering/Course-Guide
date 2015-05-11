0228538db981f6188fcbba537b2d2f230d0c445a
Matthew Keas <matthiasak@gmail.com>
2015-05-11 17:51:39 -0500

    removing shellscripts

--- open-githubs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-usernames=("aallen-dev" "bmagnantb" "arbolkiri" "dorshinho" "Jrharper0592" "cjros" "pmarsh41" "maprules1000" "firehawk09" "kawill")
-
-for i in "${usernames[@]}"
-do
-    :
-    sleep .25
-    open "https://github.com/$i"
-done
