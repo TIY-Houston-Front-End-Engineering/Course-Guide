668450fa0308acd25d1650461a75100c8afc499b
Matthew Keas <matthiasak@gmail.com>
2015-05-22 10:45:58 -0500

    moving to underscore

--- extras/setup/server.js
+++ extras/setup/server.js
@@ -7,26 +7,19 @@ function startServer() {
         http = require('http'),
         path = require('path'),
         app = express(),
-        request = require('request'),
-        _ = require('underscore');
+        request = require('request')
 
     function querify(queryParamsObject){
-        return '?'+_.map(queryParamsObject || {}, function(val, key){
-            return key+'='+val
-        }).join('&');
+        return Object.keys(queryParamsObject).map(function(val, key){ return key+'='+val }).join('&')
     }
 
     // adds a new rule to proxy a localUrl -> webUrl
     // i.e. proxify ('/my/server/google', 'http://google.com/')
     function proxify(localUrl, webUrl){
         app.get(localUrl, function(req, res) {
-            var url = [
-                webUrl,
-                querify(req.query)
-            ].join("");
-
-            req.pipe(request(url)).pipe(res);
-        });
+            var url = [ webUrl, querify(req.query) ].join("")
+            req.pipe(request(url)).pipe(res)
+        })
     }
 
     // add your proxies here.
@@ -36,12 +29,12 @@ function startServer() {
     // proxify('/brewery/styles', 'https://api.brewerydb.com/v2/styles');
 
     // all environments
-    app.set('port', process.argv[3] || process.env.PORT || 3000);
-    app.use(express.static(path.join(__dirname, '')));
+    app.set('port', process.argv[3] || process.env.PORT || 3000)
+    app.use(express.static(path.join(__dirname, '')))
 
     http.createServer(app).listen(app.get('port'), function() {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
+        console.log('Express server listening on port ' + app.get('port'))
+    })
 }
 
-module.exports.startServer = startServer;
+module.exports.startServer = startServer
