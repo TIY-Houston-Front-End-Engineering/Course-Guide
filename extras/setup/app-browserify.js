2defc8fc9bfff67770c44991d201a8b5bbdb09e3
Matthew Keas <matthiasak@gmail.com>
2015-06-01 07:48:05 -0500

    es5-shim

--- extras/setup/app-browserify.js
+++ extras/setup/app-browserify.js
@@ -1,13 +1,15 @@
 "use strict";
 
+// es5 polyfills, powered by es5-shim
+require("es5-shim")
+// es6 polyfills, powered by babel
+require("babel/register")
+
 var Promise = require('es6-promise').Promise
 // just Node?
 // var fetch = require('node-fetch')
 // Browserify?
 // require('whatwg-fetch') //--> not a typo, don't store as a var
-
-// es6 polyfills, powered by babel
-require("babel/register")
 
 // other stuff that we don't really use in our own code
 // var Pace = require("../bower_components/pace/pace.js")
