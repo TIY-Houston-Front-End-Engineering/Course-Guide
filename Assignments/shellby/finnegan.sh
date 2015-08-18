25c0dbc373671b3277dd11653c54ea65950a956c
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2015-08-18 18:11:57 -0500

    added finnegan script

---
+++ Assignments/shellby/finnegan.sh
@@ -0,0 +1,12 @@
+mkdir finnegan
+cd finnegan
+mkdir words_and_numbers
+mkdir backup_words
+cd words_and_numbers
+touch peacefugle.word
+touch pixylighting.word
+touch peri-potmother.word
+echo "a parody's bird, a peripotmother, a pringlpik in the ilandiskippy" > peri-potmother.word
+touch zero.number
+cp *.word ../backup_words/
+echo "done"
