55c0c4fa985b90f347f4470f1a8e98e4aecfb92b
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2016-06-02 15:21:04 -0500

    fixed parentheses on part 0

--- Assignments/js-fundamentals/loops-iterables-pt2.js
+++ Assignments/js-fundamentals/loops-iterables-pt2.js
@@ -1,8 +1,8 @@
 // PART 0: Write a function called squareDance() that squares each number in an array.
 
-console.assert(squareDance([1, 2][1]) === 4)
-console.assert(squareDance([5,10,15][2]) === 225)
-console.assert(squareDance([3,6,9,3][0]) === 0)
+console.assert(squareDance([1, 2])[1] === 4)
+console.assert(squareDance([5,10,15])[2] === 225)
+console.assert(squareDance([3,6,9,3])[0] === 0)
 
 // PART 1: write a function called nicer(). It should clean up the language in its input sentence.
 
